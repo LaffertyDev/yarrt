1b3fa40d243a9889c06f4290d04eb9b93b93d8c1
Nathan Lafferty <nslafferty@gmail.com>
2019-03-14 12:34:51 -0500

    added ppm image output from chap1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let num_rows = 200;
+    let num_cols = 100;
+    println!("P3\n{} {}\n255", num_rows, num_cols);
+    for y in (0..num_cols).rev() {
+        for x in 0..num_rows {
+            let r = x as f32 / num_rows as f32;
+            let g = y as f32 / num_cols as f32;
+            let b = 0.2f32;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
